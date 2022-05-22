c0f4fe380033a37233943d71f648a526e0b697a9
Andriy Kytsun <andriy.kytsun@NeoviciAB.onmicrosoft.com>
2022-05-22 21:43:20 +0300

    - fix agree stuff

--- src/app/models/addDossierPageDto.ts
+++ src/app/models/addDossierPageDto.ts
@@ -1,17 +1,18 @@
 export class AddDossierPageDto {
-    lastName: string = '';
-    firstName: string = '';
-    thirdName: string = '';
-    imageInput: any;
-    position: string  = '';
-    placeOfWork: string = '';
-    region: string = '';
-    district: string = '';
-    localCommunity: string = '';
-    fileText: any;
-    contactInfo: string = '';
-    phone: string = '';
-    email: string = '';
-    text: string = '';
-  
-  }
+  lastName: string = '';
+  firstName: string = '';
+  thirdName: string = '';
+  imageInput: any;
+  position: string = '';
+  placeOfWork: string = '';
+  region: string = '';
+  district: string = '';
+  localCommunity: string = '';
+  fileText: any;
+  contactInfo: string = '';
+  phone: string = '';
+  email: string = '';
+  text: string = '';
+  agreeForData: boolean = false;
+  agreeForContract: boolean = false;
+}
