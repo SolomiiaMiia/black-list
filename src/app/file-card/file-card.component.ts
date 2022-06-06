56cba68956a302de812c7e1f0a94f918ebd095fa
MI <brigitska.solomiya@gmail.com>
2022-06-06 14:43:52 +0300

    upd short dossier on the main page

--- src/app/file-card/file-card.component.ts
+++ src/app/file-card/file-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FullNameDossierDto } from '../models/fullNameDossierDto';
 
 @Component({
   selector: 'app-file-card',
@@ -7,25 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileCardComponent implements OnInit {
 
-public files:Files[] = [
-  { 
-    header: 'Header',
-    title: 'Title',
-    description: 'Some quick example text to build on the card title and make up the bulk of the cards content.'
-  }
+
+  
+
+public  dossierShort: FullNameDossierDto [] = [
+
+  {
+    id: 1,
+    fullName: 'Садовий Андрій Іванович',
+    date: new Date(),
+  },
+  {
+    id: 2,
+    fullName: 'Порошенко Петро Олексійович',
+    date: new Date(),
+  },
+  {
+    id: 3,
+    fullName: 'Янукович Віктор Федорович',
+    date: new Date(),
+  }, 
+  {
+    id: 4,
+    fullName: 'Тимошенко Юлія Володимирівна',
+    date: new Date(),
+  },
+  {
+    id: 4,
+    fullName: 'Медведчук Віктор Володимирович',
+    date: new Date(),
+  },
 ]
 
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
   }
 
-
-}
-
-export class Files {
-  public header: string = ''
-  public title: string = '';
-  public description: string = '';
 }
---
+++ src/app/models/fullNameDossierDto.ts
@@ -0,0 +1,6 @@
+export class FullNameDossierDto {
+    public id: number = 0;
+    public fullName: string = '';
+    public date: Date = new Date();
+  }
+  
