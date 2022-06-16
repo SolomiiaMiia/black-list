eaa707588292d9201609710308f838f912690cfb
Andriy Kytsun <andriy.kytsun@NeoviciAB.onmicrosoft.com>
2022-06-16 20:36:33 +0300

    - not found updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -55,10 +55,11 @@ const routes: Routes = [
     path: '404',
     loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule)
   },
+  { path: '**', redirectTo: '/404' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/layout/the-footer/the-footer.component.ts
+++ src/app/layout/the-footer/the-footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TheFooterComponent implements OnInit {
 
+  public Year: number = new Date().getFullYear();
   constructor() { }
 
   ngOnInit(): void {
