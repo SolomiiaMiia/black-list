85c742231455438d222b93bc99e042a18ae94838
Andriy Kytsun <andriy.kytsun@NeoviciAB.onmicrosoft.com>
2022-06-16 12:20:46 +0300

    - update api for linux

--- api/CookingApi.Web/Program.cs
+++ api/CookingApi.Web/Program.cs
@@ -9,18 +9,20 @@ using CookingApi.Web.Filters;
 using CookingApi.Web.Helpers;
 using CookingApi.Web.Middlewares;
 using Microsoft.AspNetCore.Http.Json;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseContentRoot(Directory.GetCurrentDirectory());
+builder.WebHost.UseWebRoot("wwwroot");
+builder.WebHost.UseUrls(builder.Configuration.GetSection("Kestrel:Urls").Value);
+
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(builder =>
-   {
-        //builder.WithOrigins("http://localhost:800").AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-        //builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost");
-        //builder.SetIsOriginAllowed(origin => true);
-    });
+  options.AddDefaultPolicy(builder =>
+ {
+   builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+ });
 });
 
 builder.Services.AddControllers(options =>
@@ -34,10 +36,11 @@ builder.Services.Configure<JsonOptions>(options =>
   options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
 });
 
-var dbConnectionName = Environment.GetEnvironmentVariable("DB_CONNECTION_NAME");
+
+var dbConnectionName = builder.Configuration.GetSection("ConnectionStrings:name").Value;
 
 if (string.IsNullOrWhiteSpace(dbConnectionName))
-    throw new Exception("db_connection_name_missing");
+  throw new Exception("db_connection_name_missing");
 
 NHibernateHelper.Initialize(dbConnectionName, builder);
 
@@ -49,11 +52,26 @@ HttpContextExtensions.AddHttpContextAccessor(builder.Services);
 
 builder.Services.Configure<UserOptions>(builder.Configuration.GetSection("Users"));
 
+builder.Services.AddHsts(options =>
+            {
+              options.Preload = true;
+              options.IncludeSubDomains = true;
+              options.MaxAge = TimeSpan.FromDays(365);
+            });
+
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseMiddleware<ErrorHandlerMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+  ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+
 app.UseHttpContext();
-app.UseAuthorization();
 app.UseCors();
 app.MapControllers();
 app.Run();
