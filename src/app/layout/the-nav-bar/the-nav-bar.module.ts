f7beb5a2c563db533a325c1042151eb3021b67d9
Andriy Kytsun <andriy.kytsun@NeoviciAB.onmicrosoft.com>
2022-06-16 20:53:25 +0300

    - burger collapsing

--- src/app/layout/the-nav-bar/the-nav-bar.module.ts
+++ src/app/layout/the-nav-bar/the-nav-bar.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from "@angular/router";
 import { TheNavBarComponent } from './the-nav-bar.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 
 
@@ -9,7 +10,8 @@ import { TheNavBarComponent } from './the-nav-bar.component';
   declarations: [TheNavBarComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    CollapseModule
   ],
   exports:[ TheNavBarComponent]
 })
