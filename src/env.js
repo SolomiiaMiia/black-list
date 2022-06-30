d4241a89ff56512ee602122ff77c1a3fd7c4c3dd
Andriy Kytsun <andriy.kytsun@NeoviciAB.onmicrosoft.com>
2022-06-30 16:05:44 +0300

    - upd prod api

--- src/env.js
+++ src/env.js
@@ -1,9 +1,9 @@
 (function (window) {
   window.__env = window.__env || {};
 
-  window.__env.production = false;
+  window.__env.production = true;
 
     // API url
-  window.__env.apiUrl = 'http://207.180.243.167:801/api';
+  window.__env.apiUrl = 'https://api.blacklist.ua/api';
  
 }(this));
